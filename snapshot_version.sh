a2fad0b314232607116f45cfef1bfc5ca7d30a8a
Troels Liebe Bentsen <tlb@rapanden.dk>
2015-10-30 16:22:58 +0100

    Create snapshot build script

---
+++ snapshot_version.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 1" >&2
+  exit 1
+fi
+
+export CURRENT_VERSION_SNAPSHOT=$(mvn help:evaluate  -Dexpression=project.version |egrep -v "(^\[INFO\])")
+export CURRENT_VERSION=$(expr "$CURRENT_VERSION_SNAPSHOT" : "\([0-9\.]*\)")
+export RELEASE_VERSION=$CURRENT_VERSION.$1-SNAPSHOT
+echo $RELEASE_VERSION
+mvn versions:set -DnewVersion=$RELEASE_VERSION
