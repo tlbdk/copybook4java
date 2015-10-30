733ef967d1a7a2a5c07d76945c66e2218082deef
Troels Liebe Bentsen <tlb@rapanden.dk>
2015-10-30 16:32:17 +0100

    Convert to shell syntax instead of bash

--- snapshot_version.sh
+++ snapshot_version.sh
@@ -9,4 +9,4 @@ export CURRENT_VERSION_SNAPSHOT=$(mvn help:evaluate  -Dexpression=project.versio
 export CURRENT_VERSION=$(expr "$CURRENT_VERSION_SNAPSHOT" : "\([0-9\.]*\)")
 export RELEASE_VERSION=$CURRENT_VERSION.$1-SNAPSHOT
 echo $RELEASE_VERSION
-mvn versions:set -DnewVersion=$RELEASE_VERSION
+mvn versions:set -DnewVersion=$RELEASE_VERSION -DgenerateBackupPoms=false -DallowSnapshots=true
