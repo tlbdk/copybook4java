87e49337e242a43711755b7e9a152738f3f6acb6
Troels Liebe Bentsen <tlb@nversion.dk>
2015-10-31 16:15:23 +0100

    Simplfy the version parsing by using maven for it

--- snapshot_version.sh
+++ snapshot_version.sh
@@ -5,10 +5,5 @@ if [ "$#" -ne 1 ]; then
   exit 1
 fi
 
-export CURRENT_VERSION_SNAPSHOT=$(mvn help:evaluate  -Dexpression=project.version |egrep -v "(^\[INFO\])")
-echo "org: $CURRENT_VERSION_SNAPSHOT"
-export CURRENT_VERSION=$(expr "$CURRENT_VERSION_SNAPSHOT" : "\([0-9\.]*\)")
-echo "cur: $CURRENT_VERSION"
-export RELEASE_VERSION=$CURRENT_VERSION.$1-SNAPSHOT
-echo "new: $RELEASE_VERSION"
-mvn versions:set -DnewVersion=$RELEASE_VERSION -DgenerateBackupPoms=false -DallowSnapshots=true
+export NEWVERSION='${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.incrementalVersion}'.$1-SNAPSHOT
+mvn build-helper:parse-version versions:set -DnewVersion=${NEWVERSION} versions:commit
