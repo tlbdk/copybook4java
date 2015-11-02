5b7f09b198df2dbf7b67ba8225f332a65e3bce1f
Troels Liebe Bentsen <tlb@rapanden.dk>
2015-11-02 08:47:13 +0100

    Fix rename

--- src/test/java/dk/nversion/ByteUtilsTest.java
+++ src/test/java/dk/nversion/ByteUtilsTest.java
@@ -7,7 +7,7 @@ import org.junit.rules.ExpectedException;
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
-public class TestByteUtilsTest {
+public class ByteUtilsTest {
 
     @Rule
     public ExpectedException expectedEx = ExpectedException.none();
